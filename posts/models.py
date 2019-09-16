cd3dcd4c3e0e654466ab24ed99380e1b56af8677
kimpea <kim.pearton@live.co.uk>
2019-09-16 15:51:44 +0100

    Add user to Post

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=254, default=None)
     body = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
