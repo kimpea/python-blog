95f984764e5d9d5288ca183583278b273a98ee76
kimpea <kim.pearton@live.co.uk>
2019-09-16 12:27:54 +0100

    Create view for index

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import auth, messages
 
 # Create your views here.
+def index(request):
+    """ A view that renders the index page """
+    return render(request, "index.html")
