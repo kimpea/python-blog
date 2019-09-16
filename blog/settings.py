2fae501e86108898cefdeb624929ef7cdcf037eb
kimpea <kim.pearton@live.co.uk>
2019-09-16 15:53:00 +0100

    Auth

--- blog/settings.py
+++ blog/settings.py
@@ -103,6 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
