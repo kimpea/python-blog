d46447fdfb0d1213beb7c21a21c1d65d17da4bfc
kimpea <kim.pearton@live.co.uk>
2019-09-17 11:22:09 +0100

    Add message storage

--- blog/settings.py
+++ blog/settings.py
@@ -129,3 +129,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
