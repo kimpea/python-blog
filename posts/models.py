634f7348105f09ec5b7ea45469cb467e59d7a8ba
kimpea <kim.pearton@live.co.uk>
2019-09-17 19:46:40 +0100

    Add topic

--- posts/models.py
+++ posts/models.py
@@ -3,10 +3,20 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Topic(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=254, default=None)
     body = models.TextField()
+    topic = models.ForeignKey(Topic, default="", on_delete=models.SET_DEFAULT)
     date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, default="", on_delete=models.CASCADE)
 
     def __str__(self):
