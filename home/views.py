17107c9ec05ac36e96ba4917577b8d4c5b2022c6
kimpea <kim.pearton@live.co.uk>
2019-09-17 19:46:17 +0100

    Set number of recent posts that will show on index

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import auth, messages
+from posts.models import Post
 
 # Create your views here.
 def index(request):
     """ A view that renders the index page """
-    return render(request, "index.html")
+    posts = Post.objects.all().order_by('-date_created')[:3]
+    return render(request, "index.html", {"posts": posts,})
