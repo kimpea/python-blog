f9fb687b053fff61a51496a58bda39f0093c66bd
kimpea <kim.pearton@live.co.uk>
2019-09-20 14:11:10 +0100

    Add default none to topic

--- posts/models.py
+++ posts/models.py
@@ -14,7 +14,7 @@ class Topic(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=254, default=None)
     body = models.TextField()
-    topic = models.ForeignKey(Topic, default="", on_delete=models.SET_DEFAULT)
+    topic = models.ForeignKey(Topic, default="None", on_delete=models.SET_DEFAULT)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, default="", on_delete=models.CASCADE)
