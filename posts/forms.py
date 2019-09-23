f669066dfe6dcd412a23a2b1d9a436e23b138a39
kimpea <kim.pearton@live.co.uk>
2019-09-23 16:58:18 +0100

    Create comment form

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Post
+from .models import Post, PostComment
 
 class AddPostForm(forms.ModelForm):
     class Meta: 
         model = Post
         fields = ('title', 'body', 'topic')
+
+class PostCommentForm(forms.ModelForm):
+    comment = forms.CharField(widget=forms.Textarea(
+        attrs={'rows': '4', 'cols': '5'}))
+
+    class Meta:
+        model = PostComment
+        fields = ['comment']
