f672d0598686b1c21f7fa294d6ba942e3681b3b2
kimpea <kim.pearton@live.co.uk>
2019-09-24 16:03:41 +0100

    Import Topic, add topics

--- home/views.py
+++ home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import auth, messages
-from posts.models import Post
+from posts.models import Post, Topic
 
 # Create your views here.
 def index(request):
     """ A view that renders the index page """
     posts = Post.objects.all().order_by('-date_created')[:3]
+    topics = Topic.objects.all()
     return render(request, "index.html", {
         "posts": posts,
+        "topics": topics
     })
